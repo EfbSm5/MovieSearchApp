405a54c00ecc2366ba205958b7eb06d78240a4c8
EfbSm5 <3325989547@qq.com>
2024-04-27 14:50:24 +0800

    改进了一些细节（其实并没有），新增了更多bug，现存问题：敢滚动我就敢退回上一个activity

--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -2,11 +2,13 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -30,13 +32,17 @@ public class SecondActivity extends AppCompatActivity {
     private ArrayList<String> Title = new ArrayList<>();
     private ArrayList<String> Poster = new ArrayList<>();
     private ArrayList<String> Year = new ArrayList<>();
+    private String[] title;
+    private String[] year;
+    private String[] poster;
     private String MovieName = "no data";
     private ArrayList<String> historyInput = new ArrayList<>();
     private int currentPage = 0;
-    private String[] tempOutput = new String[10];
-    private String[] title;
+
     private boolean sign = false;
-    ArrayAdapter<String> adapter;
+    private boolean sign1 = false;
+
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,14 +54,9 @@ public class SecondActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        ListView list_view = (ListView) findViewById(R.id.list_view);
         sendRequestWithHttpURLConnection();
-        title = Title.toArray(new String[Title.size()]);
-        String[] year = Year.toArray(new String[Year.size()]);
-        String[] poster = Poster.toArray(new String[Poster.size()]);
+        ListView list_view = (ListView) findViewById(R.id.list_view);
         loadMoreData();
-        adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, tempOutput);
         list_view.setAdapter(adapter);
         list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -73,8 +74,11 @@ public class SecondActivity extends AppCompatActivity {
             public void onScrollStateChanged(AbsListView view, int scrollState) {
                 // 检测是否滚动到列表底部
                 if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE
+
                         && list_view.getLastVisiblePosition() == adapter.getCount() - 1) {
                     // 滚动到底部，加载更多数据
+
+                    Log.w("TAG", "Warning level message");
                     if (sign) {
                         Toast.makeText(SecondActivity.this, "到底啦", Toast.LENGTH_SHORT);
                     } else {
@@ -90,21 +94,34 @@ public class SecondActivity extends AppCompatActivity {
 
     }
 
+    private void refreshData() {
+        title = Title.toArray(new String[Title.size()]);
+        year = Year.toArray(new String[Year.size()]);
+        poster = Poster.toArray(new String[Poster.size()]);
+    }
+
     //假如45个记录，这时max为4，此时currentPage最高可以到3,最高拉到了40个数据。title.length为45，currentPage*10为40，减去是5
     private void loadMoreData() {
+        String[] tempOutput = new String[10];
+        refreshData();
         int i = 0;
         ArrayList<String> tempOutput1 = new ArrayList<>();
         while (i < 10) {
-            if (title[currentPage * 10 + i].isEmpty()) {
-                sign = true;
-                break;
-            } else {
-                tempOutput1.add(title[currentPage * 10 + i]);
+            while (title[currentPage * 10 + i].isEmpty()) {
+                if (sign1) {
+                    sign = true;
+                    break;
+                } else {
+                    refreshData();
+                }
             }
+            tempOutput1.add(title[currentPage * 10 + i]);
             i++;
         }
         tempOutput = tempOutput1.toArray(new String[tempOutput1.size()]);
         currentPage++;
+        adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, tempOutput);
         adapter.notifyDataSetChanged();
     }
 
@@ -119,12 +136,6 @@ public class SecondActivity extends AppCompatActivity {
                 BufferedReader reader = null;
                 try {
                     URL url = new URL("https://www.omdbapi.com/?apikey=7852f5f&s=" + receivedata);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(SecondActivity.this, "Title", Toast.LENGTH_SHORT);
-                        }
-                    });
                     connection = (HttpURLConnection) url.openConnection();
                     connection.setRequestMethod("GET");
                     connection.setConnectTimeout(8000);
@@ -170,6 +181,7 @@ public class SecondActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        sign1 = true;
     }
 
 }
